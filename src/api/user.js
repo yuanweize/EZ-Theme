066fae8452f5e3dd1904dc8ee56708e9d0b5e6ab
PangHu-Code <isxieyi@outlook.com>
2026-01-28 08:30:14 +0800

    fix(apiUser)：获取用户IP接口调整

--- src/api/user.js
+++ src/api/user.js
@@ -1,86 +1,170 @@
-﻿
-import request from './request';
-
-
-export function getUserInfo() {
-  return request({
-    url: '/user/info',
-    method: 'get'
-  });
-}
-
-
-export function getIpLocationInfo() {
-  return request({
-    url: 'https://ipwho.is/',
-    method: 'get',
-    baseURL: '' 
-  });
-}
-
-
-export function redeemGiftCard(giftcard) {
-  return request({
-    url: '/user/redeemgiftcard',
-    method: 'post',
-    data: { giftcard }
-  });
-}
-
-
-export function changePassword(data) {
-  return request({
-    url: '/user/changePassword',
-    method: 'post',
-    data
-  });
-}
-
-
-export function resetSecurity() {
-  return request({
-    url: '/user/resetSecurity',
-    method: 'get'
-  });
-}
-
-
-export function updateRemindSettings(data) {
-  return request({
-    url: '/user/update',
-    method: 'post',
-    data
-  });
-}
-
-
-export function getActiveSession() {
-  return request({
-    url: '/user/getActiveSession',
-    method: 'get'
-  });
-}
-
-
-export function getCommConfig() {
-  return request({
-    url: '/user/comm/config',
-    method: 'get'
-  });
-}
-
-
-export function getTelegramBotInfo() {
-  return request({
-    url: '/user/telegram/getBotInfo',
-    method: 'get'
-  });
-}
-
-
-export function getUserSubscribe() {
-  return request({
-    url: '/user/getSubscribe',
-    method: 'get'
-  });
-} 
+﻿
+
+import request from './request';
+
+
+
+
+
+export function getUserInfo() {
+
+  return request({
+
+    url: '/user/info',
+
+    method: 'get'
+
+  });
+
+}
+
+
+
+
+
+export function getIpLocationInfo() {
+
+  return request({
+
+    url: 'https://ipinfo.io/json/',
+
+    method: 'get',
+
+    baseURL: ''
+  });
+
+}
+
+
+
+
+
+export function redeemGiftCard(giftcard) {
+
+  return request({
+
+    url: '/user/redeemgiftcard',
+
+    method: 'post',
+
+    data: { giftcard }
+
+  });
+
+}
+
+
+
+
+
+export function changePassword(data) {
+
+  return request({
+
+    url: '/user/changePassword',
+
+    method: 'post',
+
+    data
+
+  });
+
+}
+
+
+
+
+
+export function resetSecurity() {
+
+  return request({
+
+    url: '/user/resetSecurity',
+
+    method: 'get'
+
+  });
+
+}
+
+
+
+
+
+export function updateRemindSettings(data) {
+
+  return request({
+
+    url: '/user/update',
+
+    method: 'post',
+
+    data
+
+  });
+
+}
+
+
+
+
+
+export function getActiveSession() {
+
+  return request({
+
+    url: '/user/getActiveSession',
+
+    method: 'get'
+
+  });
+
+}
+
+
+
+
+
+export function getCommConfig() {
+
+  return request({
+
+    url: '/user/comm/config',
+
+    method: 'get'
+
+  });
+
+}
+
+
+
+
+
+export function getTelegramBotInfo() {
+
+  return request({
+
+    url: '/user/telegram/getBotInfo',
+
+    method: 'get'
+
+  });
+
+}
+
+
+
+
+
+export function getUserSubscribe() {
+
+  return request({
+
+    url: '/user/getSubscribe',
+
+    method: 'get'
+
+  });
+
+}
